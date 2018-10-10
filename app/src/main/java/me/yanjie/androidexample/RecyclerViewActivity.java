a2b6446e83e2b8a4490a6d3196f81cabd2755c9f
16092927 <16092927@cnsuning.com>
2018-10-10 19:10:07 +0800

    'init'

---
+++ app/src/main/java/me/yanjie/androidexample/RecyclerViewActivity.java
@@ -0,0 +1,33 @@
+package me.yanjie.androidexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class RecyclerViewActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private String[] myDataset = {"Cheese", "Pepperoni", "Black Olives"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.rvAnimals);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new RecyclerViewadapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+}
---
+++ app/src/main/java/me/yanjie/androidexample/RecyclerViewadapter.java
@@ -0,0 +1,56 @@
+package me.yanjie.androidexample;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class RecyclerViewadapter extends RecyclerView.Adapter<RecyclerViewadapter.MyViewHolder>  {
+    private String[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextView;
+
+        public MyViewHolder(LinearLayout v) {
+            super(v);
+            mTextView = v.findViewById(R.id.tvAnimalName);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public RecyclerViewadapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public RecyclerViewadapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                     int viewType) {
+        // create a new view
+        LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recycler_view_row, parent, false);
+
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mTextView.setText(mDataset[position]);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
